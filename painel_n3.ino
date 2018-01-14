afce22818e9f5f55e10c46558b80c68bb99ce738
movementes <movementes@users.noreply.github.com>
2018-01-14 01:28:53 -0200

    Update painel_n3.ino

--- painel_n3.ino
+++ painel_n3.ino
@@ -1,11 +1,3 @@
-// Notas
-
-#define NOTE_C3  131
-#define NOTE_D3  147
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_G3  196
-#define NOTE_A3  220
 #define NOTE_B3  247
 #define NOTE_D4  294
 #define NOTE_F4  349
@@ -15,16 +7,11 @@ const int echoPin = 4;
 long sensor = 0;
 int flag = 0;
 
-long microsecondsToCentimeters(long microseconds)
-{
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the
-  // object we take half of the distance travelled.
+long microsecondsToCentimeters(long microseconds) {
   return microseconds / 29 / 2;
 }
 
 void setup() {
-  Serial.begin(9600);
 }
  
 void loop() {
@@ -43,10 +30,6 @@ void loop() {
 
   cm = microsecondsToCentimeters(duration);
 
-  // debug
-  //Serial.print("cm: ");
-  //Serial.println(cm);
-
   if (cm <= sensor) {
     sensor = cm;
     flag = 0;
@@ -58,23 +41,15 @@ void loop() {
     sensor = cm;
     flag = 0;
   }
-  
-  // debug
-  //Serial.print("sensor: ");
-  //Serial.println(sensor);
 
   if (sensor > 20 && sensor <= 56) {
     tone(8, NOTE_B3, 500);
-    //Serial.println("NOTE_B3");
   } else if (sensor > 56 && sensor <= 92) {
     tone(8, NOTE_D4, 500);
-    //Serial.println("NOTE_D4");
   } else if (sensor > 92 && sensor <= 130) {
     tone(8, NOTE_F4, 500);
-    //Serial.println("NOTE_F4");
   } else {
     noTone(8);
-    //Serial.println("SILENCIO");
   }
   
   delay(100);
